d9dcffcc993eed6f3d994933580d75c365f2c75d
Kristen Stroup <kristenwhitson@gmail.com>
2019-10-31 10:03:51 -0500

    add header

--- clicky-game/src/components/Game.js
+++ clicky-game/src/components/Game.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./header"
+
+function Game(){
+
+}
 
 
 
-
-
-
-export default 
+export default Game
---
+++ clicky-game/src/components/header.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Header() {
+    <div>
+        <ul className="nav justify-content-center">
+            <li className="nav-item">
+                <a className="nav-link active" href="#">Clicky Game</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Click an Image to begin</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Score: 0 | Top Score: 0</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+    </div>
+
+
+
+}
+export default Header;
