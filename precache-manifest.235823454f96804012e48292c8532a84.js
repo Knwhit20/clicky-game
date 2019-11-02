a61bcd51c66d895aed6f5faa50c7c0358824624e
Kristen Stroup <kristenwhitson@gmail.com>
2019-11-01 20:17:16 -0500

    Updates

---
+++ precache-manifest.235823454f96804012e48292c8532a84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5b069723b9b2ac53cadfe4a828aa69",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "e0745b3b58dca1dfa1e8",
+    "url": "/clicky-game/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1fbd82982a382c24bb20",
+    "url": "/clicky-game/static/js/2.74bd094f.chunk.js"
+  },
+  {
+    "revision": "e0745b3b58dca1dfa1e8",
+    "url": "/clicky-game/static/js/main.1087b957.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
